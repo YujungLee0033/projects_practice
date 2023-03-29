178e0e750186cc21fbdb383bb06411493a657e15
Yujung <yujung.0033@gmail.com>
2023-03-29 11:49:08 +0900

    first commit

---
+++ React_Tictactoe/client.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Tictactoe from "./Tictactoe";
+
+
+ReactDOM.createRoot(document.querySelector('#root')).render(<Tictactoe />);
